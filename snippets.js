458e31605759a0a823a0101a12f31d3eba29397e
XYZerion <crzen323@gmail.com>
2017-03-06 17:45:21 -0500

    3-6-17

---
+++ snippets.js
@@ -0,0 +1,68 @@
+/*
+// Functions
+
+
+function addition (number1, number2){
+		return number1 + number2;
+};
+
+var sum = addition(17, 45);
+
+alert(sum);
+
+function multi (num1, num2){
+		return num1 * num2;
+};
+
+alert (multi (45, 75));
+
+
+//Array
+
+
+var myList = ['apples', 'oranges', 'bananas'];
+
+// myList [0] == 'apples'
+// myList [1] == 'oranges'
+// myList [2] == 'bananas'
+
+myList[3] = 'pears'; // abb to array
+myList[0] = 'watermelon'; // change a value
+
+// Arrays can hold any type of values, even
+//   other arrays or functions
+
+myList.shift() // removes and returns the first element from the array
+
+myList.pop() // removes and returns the last element from the array
+
+myList.forEach(function(value, index){
+				console.log(value, index);			 
+});  // This will list what value is in which index position of the array
+
+
+//Loops
+
+
+var times =0
+
+while (times < 10){
+	console.log ('Tried it', times);
+	times++
+} // this will check the condition first before running any code
+
+do {  
+	console.log ('Tried it', times)
+	times++
+} while (times < 10); // this will always run at least one tine before its check the condition
+
+for (var i = 0; i < 10; i++){
+	console.log ('i is', i);
+}
+
+var myList = ['apples', 'oranges', 'bananas'];
+
+for (var i = 0; i < myList.length; i++){
+	console.log (myList[i]);
+}
+*/
